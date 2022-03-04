f8e0d9440ebfb484720772254c677456fe52921e
Lucien Roberts <lucienrobertsiv@gmail.com>
2022-03-04 13:40:32 -0500

    initial commit

---
+++ MechaCarChallenge.R
@@ -0,0 +1,11 @@
+library(tidyverse)
+MechaCar_df <- read.csv(file="MechaCar_mpg.csv")
+summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=MechaCar_df))
+Suspension_Coil <- read.csv(file="Suspension_Coil.csv")
+total_summary <- Suspension_Coil %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI), .groups='keep')
+lot_summary <- Suspension_Coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI), .groups='keep')
+
+plt1 <- ggplot(Suspension_Coil, aes(y=PSI))
+plt1 + geom_boxplot()
+plt2 <- ggplot(Suspension_Coil, aes(x=Manufacturing_Lot,y=PSI))
+plt2 + geom_boxplot()
